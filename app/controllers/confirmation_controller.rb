374531cef38349e5c881fba2c8a0b69a7ed203f2
Andrew Warzocha <andrew.warzocha@gmail.com>
2020-12-16 18:49:27 -0500

    Filename Fix

--- app/controllers/confirmation_controller.rb
+++ app/controllers/confirmation_controller.rb
@@ -1,4 +1,4 @@
-class ConfirmationsController < Devise::ConfirmationsController
+class ConfirmationController < Devise::ConfirmationsController
 
     private
       def after_confirmation_path_for(resource_name, resource)
