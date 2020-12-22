57b9768eeb74b3b864ec2a483b3e7a8b352d00d5
Andrew Warzocha <andrew.warzocha@gmail.com>
2020-12-21 21:17:52 -0500

    Character Page Cleanup

--- app/controllers/admin/characters_controller.rb
+++ app/controllers/admin/characters_controller.rb
@@ -5,37 +5,35 @@ class Admin::CharactersController < AdminController
   end
 
   def new
-    setup_character_lists
     @character = Character.new
+    @user = User.find(params[:user_id])
   end
 
   def show
-    @user = User.find(params[:user_id])
     @character = Character.find(params[:id])
   end
 
   def create
     @character = Character.new(character_params)
     @character.user_id = params[:user_id]
-    if @character.save
+
+    if @character.save!
       redirect_to admin_user_path(params[:user_id])
     else
-      setup_character_lists
       render 'new'
     end
   end
 
   def edit
     @character = Character.find(params[:id])
-    @character.user_id = params[:user_id]
-    setup_character_lists
+
   end
 
   def update
     @character = Character.find(params[:id])
 
     if @character.update(character_params)
-      redirect_to admin_character_path(@character)
+      redirect_to admin_user_character_path(@character)
     else
       render 'edit'
     end
@@ -51,14 +49,7 @@ class Admin::CharactersController < AdminController
   private
 
   def character_params
-    params.require(:character).permit(:name, :pronouns, :deity_id, :race_id, :characterclass_id, :user_id)
+    params.require(:character).permit(:name, :pronouns, :deity_id, :race_id, :characterclass_id, :user_id, :level)
   end
 
-  def setup_character_lists
-    @user = User.find(params[:user_id])
-    @classes = Characterclass.select { |x| x.playeravailable == true }
-    @races = Race.select { |x| x.playeravailable == true }
-    @deities = Deity.select { |x| x.playeravailable == true }
-  end
-  
 end
