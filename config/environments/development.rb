53c47e327a82ac3478513ca86189f5526f91f123
Andrew Warzocha <andrew.warzocha@gmail.com>
2021-01-03 14:27:25 -0500

    Fix link

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'mythlarp.com' }
+  config.action_mailer.default_url_options = { :host => 'mythlarp.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.gmail.com',
   :port           => '587',
