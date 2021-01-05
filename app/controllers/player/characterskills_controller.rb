edf4379ad98a3d1e4915a2fa96413e8f010a160b
Andrew Warzocha <andrew.warzocha@gmail.com>
2021-01-04 22:38:10 -0500

    Fixing double skills and Wizard bug

--- app/controllers/player/characterskills_controller.rb
+++ app/controllers/player/characterskills_controller.rb
@@ -7,8 +7,6 @@ class Player::CharacterskillsController < PlayerController
 
     @character = Character.find_by(id: params[:character_id])
     @favoredfoes = [ 'Beasts', 'Constructs', 'Elementals', 'Monstrous Humanoids', 'Plants', 'Undead' ] - @character.characterskills.where(skill: Skill.find_by(name: 'Favored Foe')).pluck('details')
-    cpavailable = ((@character.level * 50) + 50) - (@character.skills.sum(:tier) * 10)
-    maxtier = cpavailable / 10
     @availableskills = []
     @availablegroups = []
 
@@ -16,30 +14,9 @@ class Player::CharacterskillsController < PlayerController
       skilllist = []
       
       @character.characterclass.skills.where('skills.playeravailable = true and skills.skillgroup_id = ?', skillgroup.id).each do |skill|
-        if Skillrequirement.exists?(skill: skill.id)
-          canpurchase = true
-          Skillrequirement.where(skill: skill.id).each do |r|
-            if !@character.skills.exists?(id: r.requiredskill_id)
-              canpurchase = false
-            end
-          end
-          if !canpurchase
-            next
-          end
-        end
-        if @character.skills.where(name: skill.name).count >= skill.maxpurchase
-          next
-        end
-        if skill.tier > maxtier
-          next
-        end
-        if ((skill.tier == 5) and (@character.skills.where('tier = 4').count < 2))
-          next
-        end
-        if ((skill.tier == 6) and ((@character.skills.where('tier = 4').count < 3) or (@character.skills.where('tier = 5').count < 2)))
-          next
-        end
+        if (can_purchase(@character, skill))
           skilllist.push([skill.name, skill.id]) 
+        end
       end
       if (!skilllist.empty?)
         @availableskills.push([skillgroup.name, skilllist])
@@ -54,8 +31,10 @@ class Player::CharacterskillsController < PlayerController
   def create    
     @characterskill = Characterskill.new(addskill_params)
     @character = Character.find_by(id: params[:character_id])
-
-    @characterskill.save!
+    puts('checking: ' + @characterskill.skill.name)
+    if can_purchase(@character, @characterskill.skill)
+      @characterskill.save!
+    end
     redirect_to player_character_path(id: params[:character_id])
 
   end
@@ -100,6 +79,26 @@ class Player::CharacterskillsController < PlayerController
       return true
     end
     false
+  end
+
+  def can_purchase(character, skill)
+    if Skillrequirement.exists?(skill: skill.id)
+      Skillrequirement.where(skill: skill.id).each do |r|
+        if !@character.skills.exists?(id: r.requiredskill_id)
+          return false
+        end
+      end
+    end
+    if @character.skills.where(name: skill.name).count >= skill.maxpurchase
+      return false
+    elsif skill.tier > (((@character.level * 50) + 50) - (@character.skills.sum(:tier) * 10)) / 10
+      return false
+    elsif ((skill.tier == 5) and (@character.skills.where('tier = 4').count < 2))
+      return false
+    elsif ((skill.tier == 6) and ((@character.skills.where('tier = 4').count < 3) or (@character.skills.where('tier = 5').count < 2)))
+      return false
+    end
+    return true
   end
 
 end
