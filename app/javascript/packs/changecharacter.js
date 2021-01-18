81808626825defdc749550d1ec463bf8b86c85c5
Andrew Warzocha <andrew.warzocha@gmail.com>
2021-01-17 19:26:44 -0500

    JS not Ruby Syntax

--- app/javascript/packs/changecharacter.js
+++ app/javascript/packs/changecharacter.js
@@ -14,14 +14,14 @@ function characterDetails() {
     data: { 'character_id' : character},
     success: function(data){
       $('#characterlevel').html(data.all_data.character.level)
-      document.getElementById('changeCharacterclasstoken').src = '/images/classtoken/' + data.all_data.characterclass.name.downcase + '.png'
+      document.getElementById('changeCharacterclasstoken').src = '/images/classtoken/' + data.all_data.characterclass.name.toLowerCase() + '.png'
       document.getElementById('changeCharacterclasstoken').title = data.all_data.characterclass.name;
 
       if (data.all_data.deity == null) {
         document.getElementById('changeCharacterdeitytoken').style.visibility = 'hidden';
         document.getElementById('changeCharacterdeitytoken').title = '';
       } else {
-        document.getElementById('changeCharacterdeitytoken').src = '/images/deitytoken/' + data.all_data.deity.name.downcase + '.gif'
+        document.getElementById('changeCharacterdeitytoken').src = '/images/deitytoken/' + data.all_data.deity.name.toLowerCase() + '.gif'
         document.getElementById('changeCharacterdeitytoken').style.visibility = 'visible';
         document.getElementById('changeCharacterdeitytoken').title = data.all_data.deity.name;
       }
